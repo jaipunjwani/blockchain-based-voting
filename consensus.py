aef77e299c97ffb61f363bd6296419ba858a1955
Jai Punjwani <jaipunjwani@mail.adelphi.edu>
2020-08-02 15:07:14 -0400

    further modularized consensus component

--- consensus.py
+++ consensus.py
@@ -17,6 +17,19 @@ class ConsensusParticipant:
         broadcast transaction approvals
         aggregate approved transactions into new block
     """
+
+    def get_nodes_in_agreement(self):
+        """
+        Gets all nodes in agreement with this node's perception of the blockchain.
+        """
+        nodes_in_agreement = []
+        nodes = self.node_mapping.values()
+        for node in nodes:
+            # compare hashes rather than header b/c hash is unique repr that is the same for a given block
+            if self.blockchain.current_block.hash == node.blockchain.current_block.hash:
+                nodes_in_agreement.append(node)
+        return nodes_in_agreement
+
     def begin_consensus_round(self, nodes=None):
         """
         Args:
@@ -28,36 +41,43 @@ class ConsensusParticipant:
             try:
                 nodes.remove(self)
             except ValueError as e:
-                print(e)
+                pass
         else:
             nodes = self.node_mapping.values()
-        self.last_round_nodes = nodes
+        self.last_round_nodes = nodes  # future work: will help detect difference in nodes between previous round
         self.rejection_map = {}
-        self.send_nodes_transactions_for_consensus(nodes)
+        self.broadcast_transactions_for_consensus(nodes)
 
-    def send_nodes_transactions_for_consensus(self, nodes):
-        """Sends verified transactions to all nodes in the network specifically for the consensus round"""
+    def broadcast_transactions_for_consensus(self, nodes):
+        """Broadcasts verified transactions to all nodes in the network specifically for the consensus round"""
         for node in nodes:
-            node.check_transactions_for_consensus(self.verified_transactions)
+            node.validate_transactions_for_consensus(self.verified_transactions)
 
-    def check_transactions_for_consensus(self, transactions):
-        """Validates a collection of transactions and adjusts each one's tally during the consensus round.
+    def validate_transactions_for_consensus(self, transactions):
+        """
+        Validates a collection of transactions and votes on the validity of each during the consensus round.
         Args:
             transactions            iterable of Transactions to check
         """
         for tx in transactions:
-            # TODO - check this logic...
-            if tx in self.transaction_tally:
-                self.transaction_tally[tx] = self.transaction_tally[tx] + 1
-            else:
-                # validate transaction and set tally accordingly
-                try:
-                    if tx not in self.verified_transactions:
-                        self.validate_transaction(tx)
-                    self.transaction_tally[tx] = 1
-                except Exception as e:
-                    self.rejection_map[tx] = str(e)
-                    self.transaction_tally[tx] = 0
+            # validate transaction if not already done so and set tally accordingly
+            try:
+                if tx not in self.verified_transactions:
+                    self.validate_transaction(tx)
+                self.transaction_tally[tx] = 1
+            except Exception as e:
+                self.rejection_map[tx] = str(e)
+                self.transaction_tally[tx] = 0
+
+    def broadcast_transaction_tally(self, nodes):
+        """Broadcasts nodes tally on all transactions during consensus round."""
+        for node in nodes:
+            node.aggregate_transaction_tally(self.transaction_tally)
+
+    def aggregate_transaction_tally(self, transaction_tally):
+        """Increment transaction tally from another node for known transactions from consensus round."""
+        for tx in self.transaction_tally:
+            self.transaction_tally[tx] += transaction_tally.get(tx, 0)
 
     def finalize_consensus_round(self):
         """Finalizes block and resets state for next round"""
@@ -94,18 +114,6 @@ class ConsensusParticipant:
             except KeyError:
                 pass
 
-    def get_nodes_in_agreement(self):
-        """
-        Gets all nodes in agreement with this node's perception of the blockchain.
-        """
-        nodes_in_agreement = []
-        nodes = self.node_mapping.values()
-        for node in nodes:
-            # compare hashes rather than header b/c hash is unique repr that is the same for a given block
-            if self.blockchain.current_block.hash == node.blockchain.current_block.hash:
-                nodes_in_agreement.append(node)
-        return nodes_in_agreement
-
     @staticmethod
     def demonstrate_consensus(nodes, blockchain_name):
         """
@@ -113,54 +121,38 @@ class ConsensusParticipant:
         """
         print()
         print('Kicking off consensus round for {}'.format(blockchain_name))
-        # step 1 -- achieve consensus on last block hash (aggregate consensus stats)
-        hash_agreement = {}
+
+        peer_map = {}  # node: nodes in agreement
+
         for node in nodes:
-            h = node.blockchain.current_block.hash  # hash contains previous block header, which is signed by particular node
-            if h in hash_agreement:
-                hash_agreement[h].append(node)
-            else:
-                hash_agreement[h] = [node]
-        num_hashes = len(hash_agreement.keys())
-        majority_hash = None
-        majority_nodes_len = 0
+            # step 1 -- determine which nodes are in agreement of state
+            peers = node.get_nodes_in_agreement()
+            peer_map[node] = peers
 
-        for h in hash_agreement:
-            nodes = hash_agreement[h]
-            num_nodes = len(nodes)
+            # step 2 -- send transactions to peers for validation
+            node.begin_consensus_round(nodes=peers)
 
-            if not majority_hash:
-                majority_nodes_len = num_nodes
-                majority_hash = h
+        for node in nodes:
+            # step 3 -- broadcast tally for all transactions
+            node.broadcast_transaction_tally(nodes=peer_map[node])
 
-            elif num_nodes > majority_nodes_len:
-                majority_nodes_len = num_nodes
-                majority_hash = h
+        for node in nodes: 
+            # step 4 -- commit block of valid transactions
+            node.finalize_consensus_round()
 
-        # step 2 -- kick off consensus round only for nodes that agree with the majority
-        if majority_hash:
-            nodes = hash_agreement[majority_hash]
-            for node in nodes:
-                node.begin_consensus_round(nodes=nodes.copy())
+        # extract stats from any good node, which will have the same block as any other good node
+        for node in nodes:
+            if not node.is_adversary:
+                good_node = node  
+                break
+        node = good_node
+        num_nodes = len(node.get_nodes_in_agreement())
 
-            for node in nodes:
-                node.finalize_consensus_round()
-
-            # compile stats for each node per group
-            for node in nodes:
-                if not node.is_adversary:
-                    # use any good node to display stats since it will have the same stats as the other good nodes
-                    good_node = node  
-                    break
-            node = good_node
-            num_nodes = len(nodes)
-
-            print('Consensus among {} nodes'.format(num_nodes))
-            print('Transactions approved: {}'.format(len(node.last_round_approvals)))
-            rejection_msg = 'Transactions rejected: {}'.format(len(node.rejection_map))
-            if len(node.last_round_rejections) > 0:
-                rejected_reasons = list(set(node.rejection_map.values()))
-                #TODO: message for consensus round summary seems to have state that is unreset (transaction reasons)
-                rejection_msg = '{} Reason(s): {}'.format(rejection_msg, rejected_reasons)
-            print(rejection_msg)
+        print('Consensus among {} nodes'.format(num_nodes))
+        print('Transactions approved: {}'.format(len(node.last_round_approvals)))
+        rejection_msg = 'Transactions rejected: {}'.format(len(node.rejection_map))
+        if len(node.last_round_rejections) > 0:
+            rejected_reasons = list(set(node.rejection_map.values()))
+            rejection_msg = '{} Reason(s): {}'.format(rejection_msg, rejected_reasons)
+        print(rejection_msg)
         time.sleep(2)
